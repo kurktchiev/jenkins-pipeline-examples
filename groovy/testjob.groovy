d42a8994c68e3808835a1483feccfdcd752e68bf
Boris Kurktchiev <boris@diamanti.com>
2018-08-09 13:05:05 -0400

    lets see if we can add some empty parameters to these jobs

--- groovy/testjob.groovy
+++ groovy/testjob.groovy
@@ -14,17 +14,26 @@ def parent = Jenkins.instance
 def hazelDeployDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "hazel/Jenkinsfile")
 def hazelDeployJob = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Deploy Hazelcast")
 hazelDeployJob.definition = hazelDeployDefinition
+ParameterDefinition hazelLicenseParamDef = new PasswordParameterDefinition("HAZEL_LICENSE", "", "")
+ParametersDefinitionProperty hazelLicenseParamsDef = new ParametersDefinitionProperty(hazelLicenseParamDef)
+dockerBuildJob.addProperty(hazelLicenseParamsDef)
+ParameterDefinition hazelManCenterLicenseParamDef = new PasswordParameterDefinition("MAN_CENTER_HAZEL_LICENSE", "", "")
+ParametersDefinitionProperty hazelManCenterLicenseParamsDef = new ParametersDefinitionProperty(hazelManCenterLicenseParamDef)
+dockerBuildJob.addProperty(hazelManCenterLicenseParamsDef)
 
 def hazelDemoDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "hazel/Jenkinsfile-demo")
 def hazelDemoJob = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Demo Hazelcast")
 hazelDemoJob.definition = hazelDemoDefinition
 
 def hazelCleanUpDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "hazel/Jenkinsfile-cleanup")
-def hazelCleanUpJob = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Demo Hazelcast")
+def hazelCleanUpJob = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Clean Up Hazelcast")
 hazelCleanUpJob.definition = hazelCleanUpDefinition
 
 def dockerBuildDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkinsfile")
 def dockerBuildJob = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Build Docker Image of Jenkins")
 dockerBuildJob.definition = dockerBuildDefinition
+ParameterDefinition dockerParamDef = new PasswordParameterDefinition("PW", "", "")
+ParametersDefinitionProperty dockerParamsDef = new ParametersDefinitionProperty(dockerParamDef)
+dockerBuildJob.addProperty(dockerParamsDef)
 
 parent.reload()
