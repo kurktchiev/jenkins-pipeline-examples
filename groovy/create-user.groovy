6e176401cb352d360058aaf09326fbf74762f036
Boris Kurktchiev <boris@diamanti.com>
2018-08-09 11:16:23 -0400

    push the groovy dammit!

---
+++ groovy/create-user.groovy
@@ -0,0 +1,12 @@
+import jenkins.model.*
+import hudson.security.*
+import jenkins.security.s2m.AdminWhitelistRule
+
+def instance = Jenkins.getInstance()
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("diamanti", "Diamanti@111")
+instance.setSecurityRealm(hudsonRealm)
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+instance.setAuthorizationStrategy(strategy)
+instance.save()
+Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
