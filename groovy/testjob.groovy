ee1aca4bac4031729c8406c7d5d9e6c7be4c34ee
Boris Kurktchiev <boris@diamanti.com>
2018-08-09 12:43:27 -0400

    try and see if this works again

---
+++ groovy/testjob.groovy
@@ -0,0 +1,30 @@
+import hudson.plugins.git.*;
+import jenkins.model.*
+import jenkins.util.SystemProperties
+import com.cloudbees.plugins.credentials.impl.*
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.domains.*
+import org.csanchez.jenkins.plugins.kubernetes.*
+import org.csanchez.jenkins.plugins.*
+
+def scm = new GitSCM("https://github.com/ebalsumgo/jenkins-pipeline-examples.git")
+scm.branches = [new BranchSpec("*/master")];
+def parent = Jenkins.instance
+
+def hazelDeployDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "hazel/Jenkinsfile")
+def hazelDeployJob = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Deploy Hazelcast")
+hazelDeployJob.definition = hazelDeployDefinition
+
+def hazelDemoDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "hazel/Jenkinsfile-demo")
+def hazelDemoJob = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Demo Hazelcast")
+hazelDemoJob.definition = hazelDemoDefinition
+
+def hazelCleanUpDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "hazel/Jenkinsfile-cleanup")
+def hazelCleanUpJob = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Demo Hazelcast")
+hazelCleanUpJob.definition = hazelCleanUpDefinition
+
+def dockerBuildDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkinsfile")
+def dockerBuildJob = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Build Docker Image of Jenkins")
+dockerBuildJob.definition = dockerBuildDefinition
+
+parent.reload()
