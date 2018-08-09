ca83a681b2c80b61df5afdbcd124e1e80bb2b3d1
Boris Kurktchiev <boris@diamanti.com>
2018-08-09 11:48:38 -0400

    need to get this working right

--- groovy/sample-job.groovy
+++ groovy/sample-job.groovy
@@ -4,35 +4,6 @@ import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition
 def jobName = "test-sample-cicd"
 project = Jenkins.instance.createProject(WorkflowJob, jobName)
 def script = '''
-podTemplate(cloud: 'kcloud', label: 'main', containers: [
-    containerTemplate(
-        name: 'jnlp',
-        image: 'diamanti/jnlp-slave'
-    ),
-    containerTemplate(
-        name: 'git',
-        image: 'diamanti/git',
-        command: 'cat',
-        ttyEnabled: true
-    )],
-    volumes: [persistentVolumeClaim(claimName: 'test-cicd-pvc', mountPath: '/data-cicd' )],
-    annotations: [podAnnotation(key: "diamanti.com/endpoint0", value: '{"network":"blue","perfTier":"high"}' )]
-    ) {
-    node('main') {
-        stage('Configure') {
-          env.PATH = "${tool 'maven-3.3.9'}/bin:${env.PATH}"
-        }
-        stage('Checkout') {
-          git 'https://github.com/bertjan/spring-boot-sample'
-        }
-        stage('Build') {
-          sh 'mvn -B -V -U -e clean package'
-        }
-        stage('Archive') {
-          junit allowEmptyResults: true, testResults: '**/target/**/TEST*.xml'
-        }
-    }
-}
 '''
 def definition = new CpsFlowDefinition(script,false)
 project.definition = definition
