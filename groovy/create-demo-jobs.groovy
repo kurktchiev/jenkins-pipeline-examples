546dba2a93b8b1675dfe35ae74aa445badf5050c
Boris Kurktchiev <kurktchiev@gmail.com>
2018-09-27 18:41:22 -0400

    Update create-demo-jobs.groovy

--- groovy/create-demo-jobs.groovy
+++ groovy/create-demo-jobs.groovy
@@ -12,18 +12,18 @@ def scm = new GitSCM("https://github.com/kurktchiev/jenkins-pipeline-examples.gi
 scm.branches = [new BranchSpec("*/master")];
 def parent = Jenkins.instance
 
-def hazelDeployParameters = []
 def hazelDeployDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "hazel/Jenkinsfile")
 def hazelDeployJob = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "WIP: Deploy Hazelcast")
 hazelDeployJob.definition = hazelDeployDefinition
 
-// ParameterDefinition hazelLicenseParamDef = new StringParameterDefinition("HAZEL_LICENSE", "", "")
-// ParametersDefinitionProperty hazelLicenseParamsDef = new ParametersDefinitionProperty(hazelLicenseParamDef)
+ParameterDefinition hazelLicenseParamDef = new StringParameterDefinition("HAZEL_LICENSE", "", "")
+ParametersDefinitionProperty hazelLicenseParamsDef = new ParametersDefinitionProperty(hazelLicenseParamDef)
 // hazelDeployParameters.add(hazelLicenseParamsDef)
-// ParameterDefinition hazelManCenterLicenseParamDef = new StringParameterDefinition("MAN_CENTER_HAZEL_LICENSE", "", "")
-// ParametersDefinitionProperty hazelManCenterLicenseParamsDef = new ParametersDefinitionProperty(hazelManCenterLicenseParamDef)
+ParameterDefinition hazelManCenterLicenseParamDef = new StringParameterDefinition("MAN_CENTER_HAZEL_LICENSE", "", "")
+ParametersDefinitionProperty hazelManCenterLicenseParamsDef = new ParametersDefinitionProperty(hazelManCenterLicenseParamDef)
 // hazelDeployParameters.add(hazelManCenterLicenseParamsDef)
-// hazelDeployJob.addProperty(hazelDeployParameters)
+hazelDeployJob.addProperty(hazelManCenterLicenseParamsDef)
+hazelDeployJob.addProperty(hazelLicenseParamsDef)
 
 def hazelDemoDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "hazel/Jenkinsfile-demo")
 def hazelDemoJob = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Demo Hazelcast")
