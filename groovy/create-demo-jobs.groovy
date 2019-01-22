63d27e1be1071b1ee912544d820a74e68dea7d7f
Boris Kurktchiev <boris@diamanti.com>
2019-01-22 14:25:12 -0500

    clean up the app a bit for general examples
    Signed-off-by: Boris Kurktchiev <boris@diamanti.com>

--- groovy/create-demo-jobs.groovy
+++ groovy/create-demo-jobs.groovy
@@ -12,21 +12,21 @@ def scm = new GitSCM("https://github.com/kurktchiev/jenkins-pipeline-examples.gi
 scm.branches = [new BranchSpec("*/master")];
 def parent = Jenkins.instance
 
-def hazelDeployDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "hazel/Jenkinsfile")
-def hazelDeployJob = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Deploy Hazelcast")
-hazelDeployJob.definition = hazelDeployDefinition
-hazelDeployJob.addProperty(new ParametersDefinitionProperty([
-  new StringParameterDefinition("HAZEL_LICENSE", "changeme", "Hazelcast License"),
-  new StringParameterDefinition("MAN_CENTER_HAZEL_LICENSE", "changeme", "Hazelcast Management Center License")
-  ]))
+// def hazelDeployDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "hazel/Jenkinsfile")
+// def hazelDeployJob = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Deploy Hazelcast")
+// hazelDeployJob.definition = hazelDeployDefinition
+// hazelDeployJob.addProperty(new ParametersDefinitionProperty([
+//   new StringParameterDefinition("HAZEL_LICENSE", "changeme", "Hazelcast License"),
+//   new StringParameterDefinition("MAN_CENTER_HAZEL_LICENSE", "changeme", "Hazelcast Management Center License")
+//   ]))
 
-def hazelDemoDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "hazel/Jenkinsfile-demo")
-def hazelDemoJob = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Demo Hazelcast")
-hazelDemoJob.definition = hazelDemoDefinition
+// def hazelDemoDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "hazel/Jenkinsfile-demo")
+// def hazelDemoJob = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Demo Hazelcast")
+// hazelDemoJob.definition = hazelDemoDefinition
 
-def hazelCleanUpDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "hazel/Jenkinsfile-cleanup")
-def hazelCleanUpJob = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Clean Up Hazelcast")
-hazelCleanUpJob.definition = hazelCleanUpDefinition
+// def hazelCleanUpDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "hazel/Jenkinsfile-cleanup")
+// def hazelCleanUpJob = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Clean Up Hazelcast")
+// hazelCleanUpJob.definition = hazelCleanUpDefinition
 
 def dockerBuildDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkinsfile")
 def dockerBuildJob = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Build Docker Image of Jenkins")
